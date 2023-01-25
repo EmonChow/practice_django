7c38af0bac41b9e5993b55e890ce34a8793d96c7
Emon Chowdhury <90343790+EmonChow@users.noreply.github.com>
2023-01-25 23:34:37 +0600

    tt

--- member/views/send_sms.py
+++ member/views/send_sms.py
@@ -3,6 +3,8 @@ import requests
 from member.models import ExecutiveMember
 
 
+
+
 def sendSms(mobile: str, message: str) -> dict:
     mobile = str(mobile)[-11:]
     phone_numbers = [profile.primary_phone for profile in ExecutiveMember.objects.all()]
